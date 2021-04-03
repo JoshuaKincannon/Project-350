c8e0217113f084c4e7fa841066ee843b0a724a38
freemtyl <81699835+freemtyl@users.noreply.github.com>
2021-04-02 23:14:16 -0400

    Add files via upload

--- platformGUI/GamePlatform.java
+++ platformGUI/GamePlatform.java
@@ -6,28 +6,39 @@ public class GamePlatform {
     public static class GridPanel extends JPanel
     {
 
-
+        /**
+         *
+         */
         public GridPanel()
         {
+
             setLayout(new GridLayout (3, 2));
             setBackground(Color.green);
+            //creating buttons for each Game
+            /**
+             *
+             */
             JButton b1 = new JButton("TicTacToe ");
             JButton b2 = new JButton("Uno");
             JButton b3 = new JButton("Snake");
             JButton b4 = new JButton("Othello");
             JButton b5 = new JButton("Tetris");
+            JButton b6 = new JButton("Upload Game");
             add(b1);
             add(b2);
             add(b3);
             add(b4);
             add(b5);
+            add(b6);
         }
     }
 
 
-
-
-        public static void main(String[] args){
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
             JFrame frame = new JFrame("350Games");
             frame.setSize(500, 350);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -37,8 +48,12 @@ public class GamePlatform {
 
 
             frame.getContentPane().add(tp);
+            /***********************************************************************
+             frame.pack(); if frame pack is enacted we cannot have a custom sized
+             window
+            ***********************************************************************/
 
-            //frame.pack();
+           
             frame.setVisible(true);
 
     }
