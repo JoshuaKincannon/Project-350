da9f3697422d8582221c801a450aadeca9d9ece0
ezuruonyec <57690934+ezuruonyec@users.noreply.github.com>
2021-04-21 18:53:42 -0400

    Update Main.java

--- Tic_Tac_Toe/Main.java
+++ Tic_Tac_Toe/Main.java
@@ -1,5 +1,14 @@
- //Game directions
-        int newttt = JOptionPane.showConfirmDialog(null, "Directions: \n" +
+package CIS350Project;
+
+import javax.swing.*;
+
+/**
+ * Main method for TicTacToe
+ */
+public class MainTicTacToe {
+    public static void main(String[] args) {
+        //Game directions
+        final int newttt = JOptionPane.showConfirmDialog(null, "Directions: \n" +
                         "Please enter your name and game piece," +
                         " then choose who you would like to play against.\n" +
                         "You can enter another person's name and " +
@@ -8,5 +17,7 @@
         if(newttt == JOptionPane.CANCEL_OPTION || newttt == JOptionPane.CLOSED_OPTION){
             System.exit(0);
         }
-        TicTacToeGUI tictactoe;
-        tictactoe = new TicTacToeGUI();
+
+        new TicTacToeGUI();
+    }
+}
