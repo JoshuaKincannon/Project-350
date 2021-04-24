df2a38e29cceacb8b70791176f737c6b31d13393
JoshuaKincannon <60521297+JoshuaKincannon@users.noreply.github.com>
2021-04-23 22:41:35 -0400

    Update LeaderboardForm.java

--- Tetris/LeaderboardForm.java
+++ Tetris/LeaderboardForm.java
@@ -1,4 +1,3 @@
-
 package tetris;
 
 import java.io.FileInputStream;
@@ -7,93 +6,95 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Vector;
+import javax.swing.*;
 import javax.swing.RowSorter.SortKey;
-import javax.swing.SortOrder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 
+/**
+ * Creates the GUI of the leaderboard.
+ * Displays and the name and score of players.
+ * @author Joshua Kincannon
+ * @version 13.0.2
+ */
 public class LeaderboardForm extends javax.swing.JFrame {
+    /**
+     * This variable is the table and is public for testing.
+     */
+    public static DefaultTableModel tm;
+    private String leaderboardFile = "leaderboard";
+    private TableRowSorter<TableModel> sorter;
 
- private DefaultTableModel tm;
-         
- private String leaderboardFile = "leaderboard";
- private TableRowSorter<TableModel> sorter;
+    StartupForm showStartUp;
 
     /**
-     * Initializes the table compnenets
+     * Initializes the table Components.
      */
     public LeaderboardForm() {
+        super();
         initComponents();
         initTableData();
         initTableSorter();
     }
 
     /**
-      Opens the saved leaderboard data
+     * Opens/loads the saved leaderboard data.
+     * @throws Exception Indicates conditions that a reasonable application might want to catch.
      */
     private void initTableData() {
         Vector ci = new Vector();
         ci.add("Player");
         ci.add("Score");
-        
+
         tm = (DefaultTableModel) tblLeaderboard.getModel();
         try {
-        FileInputStream fs = new FileInputStream(leaderboardFile);
-        ObjectInputStream os = new ObjectInputStream(fs);
-        
-        tm.setDataVector((Vector<Vector>)os.readObject(), ci);
-        
-        os.close();
-        fs.close();
-        }
-        catch(Exception e) {}
+            FileInputStream fs = new FileInputStream(leaderboardFile);
+            ObjectInputStream os = new ObjectInputStream(fs);
 
+            tm.setDataVector((Vector<Vector>) os.readObject(), ci);
+            os.close();
+            fs.close();
+        } catch (Exception e) {
+        }
     }
 
     /**
-     * Sorts the scores on the leaderboard from highest to lowest
+     * Sorts the scores.
      */
-    //sorting the scores from highest to lowrest
     private void initTableSorter() {
-        
-        
+
         sorter = new TableRowSorter<>(tm);
         tblLeaderboard.setRowSorter(sorter);
         ArrayList<SortKey> keys = new ArrayList<>();
         keys.add(new SortKey(1, SortOrder.DESCENDING));
-        
-        sorter.setSortKeys(keys);
-        
-        
-    }
 
+        sorter.setSortKeys(keys);
+
+
+    }
 
     /**
-     * saving the information on the leaderbaord to a file
+     * Saves leaderboard information to a file.
+     * @throws Exception Indicates conditions that a reasonable application might want to catch.
      */
-    //saving information to the leaderboard 
-    private void saveLeaderboard() { 
-        
-        try{
-        
-        FileOutputStream fs = new FileOutputStream(leaderboardFile);
-        ObjectOutputStream os = new ObjectOutputStream(fs);
-        
-        
-        os.writeObject(tm.getDataVector());
-        
-        os.close();
-        fs.close();
-        }
-        catch(Exception e){
+    private void saveLeaderboard() {
+
+        try {
+
+            FileOutputStream fs = new FileOutputStream(leaderboardFile);
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+
+
+            os.writeObject(tm.getDataVector());
+
+            os.close();
+            fs.close();
+        } catch (Exception e) {
 
         }
-
     }
-    
-    
-  
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -113,26 +114,26 @@ public class LeaderboardForm extends javax.swing.JFrame {
         });
 
         tblLeaderboard.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
+                new Object[][] {
 
-            },
-            new String [] {
-                "Player", "Score"
-            }
+                },
+                new String[] {
+                        "Player", "Score"
+                }
         ) {
-            Class[] types = new Class [] {
-                java.lang.Integer.class, java.lang.Integer.class
+            private Class[] types = new Class[] {
+                    java.lang.Integer.class, java.lang.Integer.class
             };
-            boolean[] canEdit = new boolean [] {
-                false, false
+            private boolean[] canEdit = new boolean[] {
+                    false, false
             };
 
             public Class getColumnClass(int columnIndex) {
-                return types [columnIndex];
+                return types[columnIndex];
             }
-
+            @Override
             public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
+                return canEdit[columnIndex];
             }
         });
         jScrollPane2.setViewportView(tblLeaderboard);
@@ -140,62 +141,77 @@ public class LeaderboardForm extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(btnMainMenu2, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(20, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 416, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20))
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(20, 20, 20)
+                                .addComponent(btnMainMenu2, javax.swing.GroupLayout.PREFERRED_SIZE, 139, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap(20, Short.MAX_VALUE)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 416, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(20, 20, 20))
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(btnMainMenu2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(18, Short.MAX_VALUE))
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(20, 20, 20)
+                                .addComponent(btnMainMenu2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(20, 20, 20)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap(18, Short.MAX_VALUE))
         );
 
         pack();
         setLocationRelativeTo(null);
-    }// </editor-fold>//GEN-END:initComponents
-
+    } // </editor-fold>//GEN-END:initComponents
 
     /**
-     * Button responsible for returning tot he main menu
-     * @param evt
+     * Button to navigate to the main menu.
+     * @param evt Action event.
      */
-    private void btnMainMenu2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMainMenu2ActionPerformed
-
+    public void btnMainMenu2ActionPerformed(java.awt.event.ActionEvent evt) { //GEN-FIRST:event_btnMainMenu2ActionPerformed
+        showStartUp = new StartupForm();
         this.setVisible(false);
-        Tetris.showStartup(); 
+        Tetris.showStartup();
 
-    }//GEN-LAST:event_btnMainMenu2ActionPerformed
+    } //GEN-LAST:event_btnMainMenu2ActionPerformed
 
     /**
-     * Adding the players name to the leaderbaord
-     * @param playerName
-     * @param score
+     * Adding the players name to the leaderboard.
+     * @param playerName Name of the player.
+     * @param score Score the player obtained.
      */
     public void addPlayer(String playerName, int score) {
-        
-        tm.addRow(new Object[] {playerName, score} );
+
+        tm.addRow(new Object[] {playerName, score});
         sorter.sort();
-        
         saveLeaderboard();
         this.setVisible(true);
-        
-    }
-    
 
+    }
+
+    /**
+     * Boolean to check if the leaderboard is visible.
+     * @return
+     */
+    public boolean isLeaderboardVis() {
+        return tblLeaderboard.isVisible();
+    }
+
+    /**
+     * Accessor method to return the main menu button.
+     * @return
+     */
+    public JButton getBtnMainMenu2() {
+        return btnMainMenu2;
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnMainMenu2;
+    /**
+     * Public menu button to access the JButton.
+     */
+    public javax.swing.JButton btnMainMenu2;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable tblLeaderboard;
     // End of variables declaration//GEN-END:variables
-    }
+}
