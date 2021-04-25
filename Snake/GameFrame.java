3c150e7d77c72dc6ecb4b249bbacdbaa0c827e4d
freemtyl <81699835+freemtyl@users.noreply.github.com>
2021-04-25 12:49:44 -0400

    Update GameFrame.java

--- Snake/GameFrame.java
+++ Snake/GameFrame.java
@@ -10,7 +10,6 @@ public class GameFrame extends JFrame {
 
         this.add(new GamePanel());
         this.setTitle("Snake");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
         this.setVisible(true);
